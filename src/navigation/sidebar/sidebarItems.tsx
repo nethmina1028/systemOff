72cd9455a835ff83a6ea958dc3484bb65446b8f3
Wickramasinghe W A N P <139307869+nethmina1028@users.noreply.github.com>
2025-01-03 11:15:54 +0530

    set

---
+++ src/navigation/sidebar/sidebarItems.tsx
@@ -0,0 +1,104 @@
+import { File, Inbox, Send, Receipt, KeySquare, LucideIcon, PanelsTopLeft } from "lucide-react";
+
+export interface NavSubItem {
+  title: string;
+  path: string;
+}
+
+export interface NavMainItem {
+  title: string;
+  path: string;
+  icon?: LucideIcon;
+  isActive?: boolean;
+  subItems?: NavSubItem[];
+}
+
+export interface NavGroup {
+  id: number;
+  label: string;
+  items: NavMainItem[];
+}
+
+const basePath = "/dashboard";
+
+export const sidebarItems: NavGroup[] = [
+  {
+    id: 1,
+    label: "Overview",
+    items: [
+      {
+        title: "Dashboard",
+        path: basePath,
+        icon: PanelsTopLeft,
+        isActive: true,
+      },
+    ],
+  },
+  {
+    id: 2,
+    label: "Apps & Pages",
+    items: [
+      {
+        title: "Terminal",
+        path: `${basePath}/inbox`,
+        icon: Inbox,
+      },
+      {
+        title: "Invoice",
+        path: "#",
+        icon: Receipt,
+        subItems: [
+          { title: "List", path: `${basePath}/invoice/list-preview` },
+          { title: "View", path: `${basePath}/invoice/view` },
+          { title: "Add", path: `${basePath}/invoice/add` },
+          { title: "Edit", path: `${basePath}/invoice/edit` },
+        ],
+      },
+      {
+        title: "Suppliers",
+        path: "#",
+        icon: Receipt,
+        subItems: [
+          { title: "List", path: `${basePath}/supplier/list-preview` },
+          { title: "View", path: `${basePath}/supplier/view` },
+          { title: "Add", path: `${basePath}/supplier/add` },
+          { title: "Edit", path: `${basePath}/supplier/edit` },
+        ],
+      },
+
+
+      {
+        title: "Employees",
+        path: "#",
+        icon: KeySquare,
+        subItems: [
+          { title: "List", path: `${basePath}/employee/list-preview` },
+          { title: "View", path: `${basePath}/employee/view` },
+          { title: "Add", path: `${basePath}/employee/add` },
+          { title: "Edit", path: `${basePath}/employee/edit` },
+        ],
+      },
+      {
+        title: "Drafts",
+        path: `${basePath}/drafts`,
+        icon: File,
+      },
+      {
+        title: "Sent",
+        path: `${basePath}/sent`,
+        icon: Send,
+      },
+    ],
+  },
+  {
+    id: 3,
+    label: "Billing",
+    items: [
+      {
+        title: "Billing",
+        path: `${basePath}/billing`,
+        icon: Receipt,
+      },
+    ],
+  },
+];
